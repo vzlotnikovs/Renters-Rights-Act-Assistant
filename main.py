d15ab98bd652d50e7b0c2149009a2e8ebb169391
Vladimirs Zlotnikovs <vzlotnikovs@gmail.com>
2026-02-06 13:47:13 +1100

    adding gitignore and API key example

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-def main():
-    print("Hello from vzlotn-ae-2-5!")
+import gradio as gr
 
+def main():
+    print("Welcome to the Renter's Rights Act chatbot!")
+    def greet(name, intensity):
+        return "Hello, " + name + "!" * int(intensity)
+
+    demo = gr.Interface(
+        fn=greet,
+        inputs=["text", "slider"],
+        outputs=["text"],
+        api_name="predict"
+    )
+
+    demo.launch(share=True)
 
 if __name__ == "__main__":
     main()
